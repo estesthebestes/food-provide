7ea708b1d068c26c1a9e149e4f34fe5a6a730207
Hunter <esteshunter22@gmail.com>
2022-11-01 18:57:20 -0500

    making comments

--- pages/cart.js
+++ pages/cart.js
@@ -10,7 +10,7 @@ import dynamic from 'next/dynamic';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-//
+//this is the code for our cart
 
 function CartScreen() {
 	const router = useRouter();
@@ -20,20 +20,25 @@ function CartScreen() {
 		cart: { cartItems },
 	} = state;
 
+	// this is code to remove an item from the cart
 	const removeItemHandler = (item) => {
 		dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
 	};
 
+	// this is when a user is adding stuff to the cart, we're checking if we have "stock" ie, do we have enough donations to what the food bank is requesting
 	const updateCartHandler = async (item, qty) => {
 		const quantity = Number(qty);
 		const { data } = await axios.get(`/api/products/${item._id}`);
 		if (data.countNeeded - data.countInStock < quantity) {
+			// Hah, this is actualy a good message even though it is called a .error
 			return toast.error('We have met the need! Thank you for your donation!');
 		}
+		// we still add items to the cart
 		dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
 	};
 
 	return (
+		// this is the layout for the cart. We're creating divs for each of the cart items and then we're creating a button to remove the item from the cart
 		<Layout title='Shopping Cart'>
 			<h1 className='mb-4 text-xl '> Shopping Cart </h1>
 			{cartItems.length === 0 ? (
@@ -79,6 +84,7 @@ function CartScreen() {
 												{[
 													...Array(item.countNeeded - item.countInStock).keys(),
 												].map((x) => (
+													// this is basically trying to account for the number of items we have in the cart
 													<option key={x + 1} value={x + 1}>
 														{x + 1}
 													</option>
@@ -99,12 +105,14 @@ function CartScreen() {
 					<div className='card p-5'>
 						<ul>
 							<li>
+								{/* below we are providing a subtotal for the total price and quantity within the cart */}
 								<div className='pb-3 text-lg'>
 									Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) : $
 									{cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
 								</div>
 							</li>
 							<li>
+								{/* this redirects the user to the shipping screen */}
 								<button
 									className='primary-button w-full p-2'
 									onClick={() => router.push('login?redirect=/shipping')}
